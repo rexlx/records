007ef74d318de6e36ebff75754335572fb2af2ce
rexlx <rexreedfitzhugh@gmail.com>
2022-12-24 10:42:38 -0600

    add challenge env var

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ go build ./source/main/
 export RECORDS_CONFIG=$(pwd)/config.json
 export ZINC_API_PWD="impossiblephrase"
 export KEY_STORE="https://your-endpoint.here"
+export CHALLENGE="string"
 ./main
