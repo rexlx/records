98c45e5e061767bd8e267847d4b613c86494806b
rex <rexreedfitzhugh@gmail.com>
2023-01-06 08:12:46 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-go build ./source/main/
 export RECORDS_CONFIG=$(pwd)/config.json
 export ZINC_API_PWD="impossiblephrase"
 export KEY_STORE="https://your-endpoint.here"
-export CHALLENGE="string"
+go build ./source/main/
 ./main
